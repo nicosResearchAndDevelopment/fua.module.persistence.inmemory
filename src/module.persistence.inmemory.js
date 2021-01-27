4474db17c513a84d4a0fc31af05078fcaf4cdde8
Simon Petrac <pfoerdie@gmail.com>
2021-01-27 20:03:09 +0100

    rework in progress

--- src/module.persistence.inmemory.js
+++ src/module.persistence.inmemory.js
@@ -1,11 +1,45 @@
 const
-	// dataFactory = require('../../module.persistence/src/module.persistence.js'),
-	Dataset = require('./Dataset.js');
+    dataFactory = require('../../module.persistence/src/module.persistence.js'),
+    Dataset = require('./Dataset.js'),
+    n3 = require('n3');
+
+/**
+ * @typedef {string} TermIdentifier
+ */
 
 /**
  * @param {Iterable<Quad>} [quads]
  * @returns {Dataset}
  */
-exports.dataset = function(quads) {
-	return new Dataset(quads);
-};
+function dataset(quads) {
+    return new Dataset(quads);
+}
+
+/**
+ * @param {Dataset} that
+ * @returns {boolean}
+ */
+function isDataset(that) {
+    return that instanceof Dataset;
+}
+
+/**
+ * @param {Term} term
+ * @returns {TermIdentifier}
+ */
+function termToId(term) {
+    return n3.termToId(term);
+} // termToId
+
+/**
+ * @param {TermIdentifier} termId
+ * @returns {Term}
+ */
+function termFromId(termId) {
+    return n3.termFromId(termId, dataFactory);
+} // termFromId
+
+exports = module.exports = {
+    dataset, isDataset,
+    termToId, termFromId
+}; // exports
